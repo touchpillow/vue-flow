b99b2c0640cbbc32a3450059635b92e54b697245
touchpillow <1071721873@qq.com>
2020-05-01 11:03:29 +0800

    add

---
+++ src/components/home.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="page">home</div>
+</template>
+<script lang="ts">
+import { Vue, Component } from "vue-property-decorator";
+
+@Component
+export default class Home extends Vue {}
+</script>
+<style lang="less" scoped>
+</style>
---
+++ src/components/index.ts
@@ -0,0 +1,8 @@
+import { Vue } from "vue-property-decorator";
+import Flow from "./flow/view.vue";
+const Components = {
+  Flow
+};
+Vue.component("vue-flow", Flow);
+
+export default Components;
