61061788d8f7a48b8042733eb509b30f05c6f7d7
touchpillow <1071721873@qq.com>
2020-05-04 01:03:37 +0800

    add fix

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { Vue } from "vue-property-decorator";
 import Flow from "./components/flow/view.vue";
 import FlowItem from "./components/flow/flow_item/view.vue";
+import "./assets/common.less";
 const Components = {
   Flow,
   FlowItem,
